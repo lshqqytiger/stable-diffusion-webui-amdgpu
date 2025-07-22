d2f9a45c7c17247a8f60d0d134efa2aafb360c30
Ajay Jangir <ajayjangir5252@gmail.com>
2025-07-22 10:49:19 +0530

    repo backup: switching branch to mine

--- webui-user.bat
+++ webui-user.bat
@@ -1,7 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON="C:\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --use-directml --use-xformers
+set COMMANDLINE_ARGS= --use-directml
+
 call webui.bat
